67ef4c5658d01142304d3139d3290084309ddab7
Athos Gustavo <athosgustavowf@gmail.com>
2023-11-30 22:19:48 -0300

    revert feito

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import app from "./src/app.js";
 
 const PORT = 3000;
 
+//mongoose.connect("mongodb+srv://athosgustavowf:<password>@athosfabricio.xwn7ltv.mongodb.net/?retryWrites=true&w=majority");
+
 app.listen(PORT, () => {
   console.log("servidor escutando!");
 });
